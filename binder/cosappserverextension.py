1057f5e6220199557e198d34d821e0781b4d0b52
LE Duc Trung <leductrungxf@gmail.com>
2021-06-26 09:15:48 +0200

    Remove url prefix

--- binder/cosappserverextension.py
+++ binder/cosappserverextension.py
@@ -3,4 +3,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """Start CoSApp Lab server"""
-    Popen(["cosapp", "load", "-a", "--url_prefix=/user/.*?/proxy/6789"])
+    Popen(["cosapp", "load", "-a"])
